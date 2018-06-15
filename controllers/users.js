19f59741de64b368ab8a5c97623483eed0134b02
Technophile <patricia.snodgrass@gmail.com>
2018-06-15 10:42:06 -0500

    added events tab

--- controllers/users.js
+++ controllers/users.js
@@ -55,46 +55,56 @@ router.get("/viewuser/:user_id", isAuthenticated, function (req, res) {
                 }).then(function (subbeddata) {
                     for (let subbed of subbeddata) {
                         subbed.Subscriptions = true;
-                    }
-
-                    var hbsObject = {
-                        myProfile: (req.params.user_id == req.user.userId),
-                        profile: userdata,
-                        posts: postdata,
-                        followeds: followeddata,
-                        subscriptions: subbeddata,
                     };
-                    res.renderWithContext("viewuser", hbsObject);
+
+                    db.User.findById(req.user.userId).then(function (currentUser) {
+                        console.log("in the router, getting all events, before rendering the view ");
+                        currentUser.getEvent_ID(
+                            { include: [{ model: db.User, as: "User_Id" }, { model: db.User, as: "EventCreator" }] }
+                        ).then(function (attendEvents) {
+
+                            var hbsObject = {
+                                myProfile: (req.params.user_id == req.user.userId),
+                                profile: userdata,
+                                posts: postdata,
+                                followeds: followeddata,
+                                subscriptions: subbeddata,
+                                userAttendEvents: attendEvents
+
+                            };
+                            res.renderWithContext("viewuser", hbsObject);
+                        });
+                    });
                 });
+            })
+        });
+    });
+});
+
+        router.get("/editprofile", isAuthenticated, function (req, res) {
+            var hbsObject = {};
+            console.log(hbsObject);
+
+            res.renderWithContext("editprofile", hbsObject);
+        });
+
+        router.post("/editprofile", isAuthenticated, function (req, res) {
+            var changes = {
+                name: req.body.name,
+                avatar: req.body.avatar,
+                coverImg: req.body.coverImg,
+                rank: req.body.rank,
+                branch: req.body.branch,
+                deployment: req.body.deployment,
+                mos: req.body.mos,
+                bio: req.body.bio,
+            };
+
+            db.User.update(changes, {
+                where: { userId: req.user.userId },
+            }).then(function () {
+                res.redirect("/viewuser/" + req.user.userId);
             });
         });
-    })
-});
 
-router.get("/editprofile", isAuthenticated, function (req, res) {
-    var hbsObject = {};
-    console.log(hbsObject);
-
-    res.renderWithContext("editprofile", hbsObject);
-});
-
-router.post("/editprofile", isAuthenticated, function (req, res) {
-  var changes = {
-    name: req.body.name,
-    avatar: req.body.avatar,
-    coverImg: req.body.coverImg,
-    rank: req.body.rank,
-    branch: req.body.branch,
-    deployment: req.body.deployment,
-    mos: req.body.mos,
-    bio: req.body.bio,
-  };
-
-  db.User.update(changes, {
-    where: { userId: req.user.userId },
-  }).then(function () {
-    res.redirect("/viewuser/" + req.user.userId);
-  });
-});
-
-module.exports = router;
+        module.exports = router;
